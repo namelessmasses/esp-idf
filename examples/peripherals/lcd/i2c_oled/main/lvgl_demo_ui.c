9c674678a0fb9008c60396a7216e07203095d22e
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2026-03-06 15:39:05 -0800

    example_lvgl_ui: simple label only

--- examples/peripherals/lcd/i2c_oled/main/lvgl_demo_ui.c
+++ examples/peripherals/lcd/i2c_oled/main/lvgl_demo_ui.c
@@ -6,13 +6,14 @@
 
 #include "lvgl.h"
 
-void example_lvgl_demo_ui(lv_display_t *disp)
-{
+#include "esp_log.h"
+
+#define TAG "lvgl_demo_ui"
+
+void example_lvgl_ui(lv_display_t *disp) {
+    ESP_LOGD(TAG, "Running LVGL demo UI");
     lv_obj_t *scr = lv_display_get_screen_active(disp);
     lv_obj_t *label = lv_label_create(scr);
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
-    lv_label_set_text(label, "Hello Espressif, Hello LVGL.");
-    /* Size of the screen (if you use rotation 90 or 270, please use lv_display_get_vertical_resolution) */
-    lv_obj_set_width(label, lv_display_get_horizontal_resolution(disp));
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
+    lv_label_set_text(label, "Loaded OK.");
+    lv_obj_center(label);
 }
