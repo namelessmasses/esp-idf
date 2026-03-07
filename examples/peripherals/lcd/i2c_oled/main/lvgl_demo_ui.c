6a2de43cd435b3544262677ee264509c4654a8a6
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2026-03-07 01:39:18 -0800

    UI displays simple label
    - using style shared across components

--- examples/peripherals/lcd/i2c_oled/main/lvgl_demo_ui.c
+++ examples/peripherals/lcd/i2c_oled/main/lvgl_demo_ui.c
@@ -6,14 +6,33 @@
 
 #include "lvgl.h"
 
+#include "core/lv_obj.h"
+#include "core/lv_obj_style_gen.h"
+#include "display/lv_display.h"
 #include "esp_log.h"
+#include "misc/lv_color.h"
+#include "misc/lv_style_gen.h"
+#include "misc/lv_text.h"
 
 #define TAG "lvgl_demo_ui"
 
-void example_lvgl_ui(lv_display_t *disp) {
+static lv_style_t s_style;
+
+void example_lvgl_ui(lv_display_t *disp)
+{
     ESP_LOGD(TAG, "Running LVGL demo UI");
+
+    lv_style_set_bg_color(&s_style, lv_color_black());
+    lv_style_set_text_color(&s_style, lv_color_white());
+    lv_style_set_text_font(&s_style, &lv_font_unscii_8);
+    lv_style_set_text_align(&s_style, LV_TEXT_ALIGN_CENTER);
+
     lv_obj_t *scr = lv_display_get_screen_active(disp);
+    lv_obj_clean(scr);
+    lv_obj_add_style(scr, &s_style, LV_PART_MAIN);
+
     lv_obj_t *label = lv_label_create(scr);
+
     lv_label_set_text(label, "Loaded OK.");
     lv_obj_center(label);
 }
