e9782d0c9512e2f6d19d59c4a58e7bad0d1b8e5f
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2026-03-08 01:46:56 -0800

    i2c_basic: using SHT41 temp/rh sensor

--- examples/peripherals/i2c/i2c_basic/main/i2c_basic_example_main.c
+++ examples/peripherals/i2c/i2c_basic/main/i2c_basic_example_main.c
@@ -6,88 +6,247 @@
 /* i2c - Simple Example
 
    Simple I2C example that shows how to initialize I2C
-   as well as reading and writing from and to registers for a sensor connected over I2C.
+   as well as reading and writing from and to registers for a sensor connected
+   over I2C.
 
-   The sensor used in this example is a MPU9250 inertial measurement unit.
+   The sensor used in this example is a SHT41 inertial measurement unit.
 */
-#include <stdio.h>
-#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
+
 #include "driver/i2c_master.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
 
 static const char *TAG = "example";
 
-#define I2C_MASTER_SCL_IO           CONFIG_I2C_MASTER_SCL       /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           CONFIG_I2C_MASTER_SDA       /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM              I2C_NUM_0                   /*!< I2C port number for master dev */
-#define I2C_MASTER_FREQ_HZ          CONFIG_I2C_MASTER_FREQUENCY /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE   0                           /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE   0                           /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_TIMEOUT_MS       1000
+#define I2C_MASTER_SCL_IO                                                      \
+    CONFIG_I2C_MASTER_SCL /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO                                                      \
+    CONFIG_I2C_MASTER_SDA        /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUM_0 /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ                                                     \
+    CONFIG_I2C_MASTER_FREQUENCY     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS     1000
 
-#define MPU9250_SENSOR_ADDR         0x68        /*!< Address of the MPU9250 sensor */
-#define MPU9250_WHO_AM_I_REG_ADDR   0x75        /*!< Register addresses of the "who am I" register */
-#define MPU9250_PWR_MGMT_1_REG_ADDR 0x6B        /*!< Register addresses of the power management register */
-#define MPU9250_RESET_BIT           7
-
-/**
- * @brief Read a sequence of bytes from a MPU9250 sensor registers
- */
-static esp_err_t mpu9250_register_read(i2c_master_dev_handle_t dev_handle, uint8_t reg_addr, uint8_t *data, size_t len)
+#define SHT41_SENSOR_ADDR 0x44 /*!< Address of the SHT41 sensor */
+enum
 {
-    return i2c_master_transmit_receive(dev_handle, &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-}
-
-/**
- * @brief Write a byte to a MPU9250 sensor register
- */
-static esp_err_t mpu9250_register_write_byte(i2c_master_dev_handle_t dev_handle, uint8_t reg_addr, uint8_t data)
-{
-    uint8_t write_buf[2] = {reg_addr, data};
-    return i2c_master_transmit(dev_handle, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-}
+    CMD_READ_HIGH_PRECISION   = 0xFD,
+    CMD_READ_MEDIUM_PRECISION = 0xF6,
+    CMD_READ_LOW_PRECISION    = 0xE0,
+    CMD_READ_SERIAL_NUMBER    = 0x89,
+    CMD_HEATED_200mW_1000ms   = 0x39,
+    CMD_HEATED_200mW_100ms    = 0x32,
+    CMD_HEATED_110mW_1000ms   = 0x2F,
+    CMD_HEATED_110mW_100ms    = 0x24,
+    CMD_HEATED_20mW_1000ms    = 0x1E,
+    CMD_HEATED_20mW_100ms     = 0x15
+} sht41_command_t;
 
 /**
  * @brief i2c master initialization
  */
-static void i2c_master_init(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_handle_t *dev_handle)
+static void i2c_master_init(i2c_master_bus_handle_t *bus_handle,
+                            i2c_master_dev_handle_t *dev_handle)
 {
     i2c_master_bus_config_t bus_config = {
-        .i2c_port = I2C_MASTER_NUM,
-        .sda_io_num = I2C_MASTER_SDA_IO,
-        .scl_io_num = I2C_MASTER_SCL_IO,
-        .clk_source = I2C_CLK_SRC_DEFAULT,
-        .glitch_ignore_cnt = 7,
+        .i2c_port                     = I2C_MASTER_NUM,
+        .sda_io_num                   = I2C_MASTER_SDA_IO,
+        .scl_io_num                   = I2C_MASTER_SCL_IO,
+        .clk_source                   = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt            = 7,
         .flags.enable_internal_pullup = true,
     };
     ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, bus_handle));
 
     i2c_device_config_t dev_config = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-        .device_address = MPU9250_SENSOR_ADDR,
-        .scl_speed_hz = I2C_MASTER_FREQ_HZ,
+        .device_address  = SHT41_SENSOR_ADDR,
+        .scl_speed_hz    = I2C_MASTER_FREQ_HZ,
     };
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(*bus_handle, &dev_config, dev_handle));
+    ESP_ERROR_CHECK(
+        i2c_master_bus_add_device(*bus_handle, &dev_config, dev_handle));
+}
+
+typedef struct
+{
+    uint16_t temp_ticks;
+    float    temperature_celcius;
+    float    temperature_fahrenheit;
+    uint16_t rh_ticks;
+    float    relative_humidity;
+} sht41_data_t;
+
+static void print_sht41_data(const sht41_data_t *data)
+{
+    ESP_LOGI(TAG, "Raw temperature ticks: %u", data->temp_ticks);
+    ESP_LOGI(TAG, "Temperature: %.2f °C", data->temperature_celcius);
+    ESP_LOGI(TAG, "Temperature: %.2f °F", data->temperature_fahrenheit);
+    ESP_LOGI(TAG, "Raw humidity ticks: %u", data->rh_ticks);
+    ESP_LOGI(TAG, "Relative Humidity: %.2f %%", data->relative_humidity);
+}
+
+static void process_sht41_data(const uint8_t *raw_data, sht41_data_t *data)
+{
+    data->temp_ticks = raw_data[0] << 8 | raw_data[1];
+    data->temperature_celcius =
+        -45.f + 175.f * ((float)data->temp_ticks / 65535.f);
+    data->temperature_fahrenheit =
+        -49.f + 315.f * ((float)data->temp_ticks / 65535.f);
+
+    data->rh_ticks          = raw_data[3] << 8 | raw_data[4];
+    data->relative_humidity = -6.f + 125.f * ((float)data->rh_ticks / 65535.f);
+}
+
+typedef struct
+{
+    enum
+    {
+        POWER_20mW,
+        POWER_110mW,
+        POWER_200mW
+    } power;
+
+    enum
+    {
+        SHORT = 100,
+        LONG  = 1000
+    } duration;
+} heating_config_t;
+
+static esp_err_t sensor_get_reading(i2c_master_dev_handle_t dev_handle,
+                                    sht41_data_t           *data,
+                                    const heating_config_t *heating_config)
+{
+    uint8_t  command          = CMD_READ_HIGH_PRECISION;
+    uint16_t wait_duration_ms = 10;
+
+    if (heating_config)
+    {
+        if (heating_config->power == POWER_20mW)
+        {
+            if (heating_config->duration == LONG)
+            {
+                wait_duration_ms += 1000;
+                command = CMD_HEATED_20mW_1000ms;
+            }
+            else
+            {
+                wait_duration_ms += 100;
+                command = CMD_HEATED_20mW_100ms;
+            }
+        }
+        else if (heating_config->power == POWER_110mW)
+        {
+            if (heating_config->duration == LONG)
+            {
+                wait_duration_ms += 1000;
+                command = CMD_HEATED_110mW_1000ms;
+            }
+            else
+            {
+                wait_duration_ms += 100;
+                command = CMD_HEATED_110mW_100ms;
+            }
+        }
+        else
+        {
+            if (heating_config->duration == LONG)
+            {
+                wait_duration_ms += 1000;
+                command = CMD_HEATED_200mW_1000ms;
+            }
+            else
+            {
+                wait_duration_ms += 100;
+                command = CMD_HEATED_200mW_100ms;
+            }
+        }
+    }
+
+    ESP_ERROR_CHECK(
+        i2c_master_transmit(dev_handle, &command, 1, I2C_MASTER_TIMEOUT_MS));
+
+    vTaskDelay(pdMS_TO_TICKS(wait_duration_ms));
+
+    uint8_t raw_data[6];
+    esp_err_t res = ESP_ERR_TIMEOUT;
+    for (int i = 0; i < 3; i++)
+    {
+        res = i2c_master_receive(
+            dev_handle, raw_data, sizeof(raw_data), I2C_MASTER_TIMEOUT_MS);
+        if (res == ESP_OK)
+        {
+            break;
+        }
+
+        if (res == ESP_ERR_TIMEOUT)
+        {
+            ESP_LOGW(TAG, "I2C bus is busy, retrying in 10ms...");
+        }
+        else
+        {
+            ESP_LOGW(TAG, "Error reading from sensor: %s, retrying in 10ms...",
+                     esp_err_to_name(res));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+
+    if (res != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to read data from sensor after multiple attempts");
+        return res;
+    }
+
+    process_sht41_data(raw_data, data);
+
+    return ESP_OK;
 }
 
 void app_main(void)
 {
-    uint8_t data[2];
+    uint8_t                 data[6];
     i2c_master_bus_handle_t bus_handle;
     i2c_master_dev_handle_t dev_handle;
     i2c_master_init(&bus_handle, &dev_handle);
     ESP_LOGI(TAG, "I2C initialized successfully");
 
-    /* Read the MPU9250 WHO_AM_I register, on power up the register should have the value 0x71 */
-    ESP_ERROR_CHECK(mpu9250_register_read(dev_handle, MPU9250_WHO_AM_I_REG_ADDR, data, 1));
-    ESP_LOGI(TAG, "WHO_AM_I = %X", data[0]);
+    ESP_LOGI(TAG, "Probing SHT41 sensor");
+    esp_err_t ret = !ESP_OK;
+    do
+    {
+        ret = i2c_master_probe(bus_handle, SHT41_SENSOR_ADDR, 1000);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    } while (ret != ESP_OK);
 
-    /* Demonstrate writing by resetting the MPU9250 */
-    ESP_ERROR_CHECK(mpu9250_register_write_byte(dev_handle, MPU9250_PWR_MGMT_1_REG_ADDR, 1 << MPU9250_RESET_BIT));
+    ESP_LOGI(TAG, "Requesting serial number from SHT41 sensor");
+    uint8_t cmd_read_serial_number = CMD_READ_SERIAL_NUMBER;
+    ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, &cmd_read_serial_number, 1,
+                                        I2C_MASTER_TIMEOUT_MS));
 
-    ESP_ERROR_CHECK(i2c_master_bus_rm_device(dev_handle));
-    ESP_ERROR_CHECK(i2c_del_master_bus(bus_handle));
-    ESP_LOGI(TAG, "I2C de-initialized successfully");
+    vTaskDelay(pdMS_TO_TICKS(10));
+
+    ESP_LOGI(TAG, "Reading serial number from SHT41 sensor");
+    ESP_ERROR_CHECK(i2c_master_receive(dev_handle, data, sizeof(data),
+                                       I2C_MASTER_TIMEOUT_MS));
+
+    uint32_t serial_number =
+        (data[0] << 24) | (data[1] << 16) | (data[3] << 8) | data[4];
+
+    ESP_LOGI(TAG, "SHT41 Serial Number: 0x%08X", serial_number);
+
+    ESP_LOGI(TAG, "Requesting measurement from SHT41 sensor");
+    sht41_data_t data_struct;
+    ESP_ERROR_CHECK(sensor_get_reading(dev_handle, &data_struct, NULL));
+    print_sht41_data(&data_struct);
+
+    ESP_LOGI(TAG, "Requesting measurement with heating from SHT41 sensor");
+    heating_config_t heating_config = {.power = POWER_110mW, .duration = LONG};
+    ESP_ERROR_CHECK(sensor_get_reading(dev_handle, &data_struct, &heating_config));
+    print_sht41_data(&data_struct);
 }
