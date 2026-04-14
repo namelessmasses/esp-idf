d5d212d37d145cc744a51478a7d6be8fd8079854
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2026-04-14 16:37:09 -0700

    refactor(tests): update ads1115 for config register codec

--- examples/peripherals/lcd/i2c_oled/test/main/test_ads1115.cpp
+++ examples/peripherals/lcd/i2c_oled/test/main/test_ads1115.cpp
@@ -1,8 +1,10 @@
+#include "ads1115_i2c_codec.h"
 #include "unity.h"
 
 #include "ads1115.h"
 
-TEST_CASE("ads1115 encode address register pointer", "[ads1115]") {
+TEST_CASE("ads1115.address_register_pointer",
+          "ads1115 encode address register pointer") {
 
     ads1115_address_pointer_register_t host_reg;
     ads1115_address_pointer_register_t i2c_reg;
@@ -12,4 +14,57 @@ TEST_CASE("ads1115 encode address register pointer", "[ads1115]") {
 
     // Bits[7:2] should be 0, bits[1:0] should be the register address.
     TEST_ASSERT_EQUAL_UINT8(ADS1115_REG_CONVERSION, i2c_reg.P);
+}
+
+TEST_CASE("ads1115.address_register_pointer",
+          "ads1115 decode address register pointer") {
+
+    ads1115_address_pointer_register_t i2c_reg;
+    ads1115_address_pointer_register_t host_reg;
+
+    i2c_reg.P = ADS1115_REG_CONFIG;
+    ads1115_decode_address_register_pointer(&i2c_reg, &host_reg);
+
+    // Bits[7:2] should be ignored, bits[1:0] should be the register address.
+    TEST_ASSERT_EQUAL_UINT8(ADS1115_REG_CONFIG, host_reg.P);
+}
+
+TEST_CASE("ads1115.config_register",
+          "default host config register encodes to i2c 0x8583") {
+    ads1115_register_t config_reg_host{
+        .address = {.P = ADS1115_REG_CONFIG},
+        .config  = {.OS        = ads1115_config_OS_DEFAULT,
+                    .MUX       = ads1115_config_MUX_DEFAULT,
+                    .PGA       = ads1115_config_PGA_DEFAULT,
+                    .MODE      = ads1115_config_MODE_DEFAULT,
+                    .DR        = ads1115_config_DR_DEFAULT,
+                    .COMP_MODE = ads1115_config_COMP_MODE_DEFAULT,
+                    .COMP_POL  = ads1115_config_COMP_POL_DEFAULT,
+                    .COMP_LAT  = ads1115_config_COMP_LAT_DEFAULT,
+                    .COMP_QUE  = ads1115_config_COMP_QUE_DEFAULT}};
+
+    ads1115_register_t config_reg_i2c;
+    ads1115_encode_register(&config_reg_host, &config_reg_i2c);
+    TEST_ASSERT_EQUAL_UINT16(0x8583, config_reg_i2c.raw);
+}
+
+TEST_CASE("ads1115.config_register",
+          "default i2c config register 0x8583, decodes to expected default "
+          "config register") {
+    ads1115_register_t config_reg_i2c = {.address = {.P = ADS1115_REG_CONFIG},
+                                          .raw     = 0x8583};
+
+    ads1115_register_t config_reg_host;
+    ads1115_decode_register(&config_reg_i2c, &config_reg_host);
+
+    TEST_ASSERT_EQUAL_UINT8(ADS1115_REG_CONFIG, config_reg_host.address.P);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_OS_DEFAULT, config_reg_host.config.OS);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_MUX_DEFAULT, config_reg_host.config.MUX);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_PGA_DEFAULT, config_reg_host.config.PGA);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_MODE_DEFAULT, config_reg_host.config.MODE);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_DR_DEFAULT, config_reg_host.config.DR);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_MODE_DEFAULT, config_reg_host.config.COMP_MODE);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_POL_DEFAULT, config_reg_host.config.COMP_POL);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_LAT_DEFAULT, config_reg_host.config.COMP_LAT);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_QUE_DEFAULT, config_reg_host.config.COMP_QUE);
 }
