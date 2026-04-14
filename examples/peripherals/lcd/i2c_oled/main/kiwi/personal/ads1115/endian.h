d5ea1eefbcc6511592c38100087cfe09a14f1fac
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2026-04-14 13:54:32 -0700

    refactor(endian): Support C assert  and C++ static_assert for 16-bit input size check

--- examples/peripherals/lcd/i2c_oled/main/kiwi/personal/ads1115/endian.h
+++ examples/peripherals/lcd/i2c_oled/main/kiwi/personal/ads1115/endian.h
@@ -3,8 +3,18 @@
 #include <limits.h>
 #include <stdint.h>
 
+#if defined(__cplusplus)
+#include <cassert>
+#else
+#include <assert.h>
+#endif
+
 static inline uint16_t ads1115_endian_swap_16(uint16_t in) {
+#if defined(__cplusplus)
     constexpr size_t k_sz = sizeof(uint16_t);
     static_assert(k_sz == 2, "ads1115_endian_swap_16 requires 16-bit input");
+    #else
+    assert(sizeof(uint16_t) == 2 && "ads1115_endian_swap_16 requires 16-bit input");
+#endif
     return (in << CHAR_BIT) | (in >> CHAR_BIT);
 }
