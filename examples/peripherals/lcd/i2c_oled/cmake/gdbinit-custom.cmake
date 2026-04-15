f2d32eedafc8c13beaab83d71a3acf7152bb4fb8
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2026-04-15 06:42:19 -0700

    refactor(gdbinit): streamline GDB init script handling and paths

--- examples/peripherals/lcd/i2c_oled/cmake/gdbinit-custom.cmake
+++ examples/peripherals/lcd/i2c_oled/cmake/gdbinit-custom.cmake
@@ -1,17 +1,18 @@
-set(generated_connect_path "${CMAKE_BINARY_DIR}/gdbinit/connect")
+set(generated_connect_path "${CMAKE_BINARY_DIR}/gdbinit")
 if(NOT DEFINED GDBINIT_CONNECT_PATH)
-    set(GDBINIT_CONNECT_PATH "${CMAKE_SOURCE_DIR}/gdbinit/connect")
+    set(GDBINIT_CONNECT_PATH "${CMAKE_SOURCE_DIR}/gdbinit")
 endif()
 set(project_connect_path "${GDBINIT_CONNECT_PATH}")
 
 if(EXISTS "${project_connect_path}")
-    message(STATUS "Using GDB connect script from ${project_connect_path}")
-    file(READ "${project_connect_path}" project_connect_content)
-    file(WRITE "${generated_connect_path}"
-        "${project_connect_content}")
-    message(STATUS "Generated GDB connect script at ${generated_connect_path}")
+    message(STATUS "Using GDB init scripts from ${project_connect_path}")
+    file(REMOVE_RECURSE "${generated_connect_path}")
+    file(MAKE_DIRECTORY "${generated_connect_path}")
+    file(GLOB gdb_connect_files "${project_connect_path}/*")
+    file(COPY ${gdb_connect_files} DESTINATION "${generated_connect_path}")
+    message(STATUS "Generated GDB init script at ${generated_connect_path}")
 else()
-    message(WARNING "GDB connect script not found at ${project_connect_path}; "
-        "skipping generation of GDB connect script")
+    message(WARNING "GDB init script not found at ${project_connect_path}; "
+        "skipping generation of GDB init script")
 endif()
 
