cab163e4b568388d73c431f97aad2d52f7bd079e
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2026-04-15 00:33:12 -0700

    refactor(i2c_oled): include sdkconfig and adjust test execution logic

--- examples/peripherals/lcd/i2c_oled/test/main/test_main.c
+++ examples/peripherals/lcd/i2c_oled/test/main/test_main.c
@@ -1,6 +1,11 @@
 #include "unity.h"
 
-void app_main(void) {
+#include <sdkconfig.h>
 
+void app_main(void) {
+#if 1 || CONFIG_ESP_SYSTEM_GDBSTUB_RUNTIME
+    unity_run_all_tests();
+#else
     unity_run_menu();
+#endif
 }
