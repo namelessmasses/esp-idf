a747f91ae8969f88ca7afddb96818e4916684ec1
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2026-04-15 00:33:42 -0700

    refactor(i2c_oled): rename test cases for address and config register

--- examples/peripherals/lcd/i2c_oled/test/main/test_ads1115.cpp
+++ examples/peripherals/lcd/i2c_oled/test/main/test_ads1115.cpp
@@ -1,9 +1,7 @@
 #include "ads1115_i2c_codec.h"
 #include "unity.h"
 
-#include "ads1115.h"
-
-TEST_CASE("ads1115.address_register_pointer",
+TEST_CASE("ads1115.address_register_pointer.encode",
           "ads1115 encode address register pointer") {
 
     ads1115_address_pointer_register_t host_reg;
@@ -16,7 +14,7 @@ TEST_CASE("ads1115.address_register_pointer",
     TEST_ASSERT_EQUAL_UINT8(ADS1115_REG_CONVERSION, i2c_reg.P);
 }
 
-TEST_CASE("ads1115.address_register_pointer",
+TEST_CASE("ads1115.address_register_pointer.decode",
           "ads1115 decode address register pointer") {
 
     ads1115_address_pointer_register_t i2c_reg;
@@ -29,7 +27,7 @@ TEST_CASE("ads1115.address_register_pointer",
     TEST_ASSERT_EQUAL_UINT8(ADS1115_REG_CONFIG, host_reg.P);
 }
 
-TEST_CASE("ads1115.config_register",
+TEST_CASE("ads1115.config_register.encode.default",
           "default host config register encodes to i2c 0x8583") {
     ads1115_register_t config_reg_host{
         .address = {.P = ADS1115_REG_CONFIG},
@@ -48,23 +46,33 @@ TEST_CASE("ads1115.config_register",
     TEST_ASSERT_EQUAL_UINT16(0x8583, config_reg_i2c.raw);
 }
 
-TEST_CASE("ads1115.config_register",
+TEST_CASE("ads1115.config_register.decode.default",
           "default i2c config register 0x8583, decodes to expected default "
           "config register") {
     ads1115_register_t config_reg_i2c = {.address = {.P = ADS1115_REG_CONFIG},
-                                          .raw     = 0x8583};
+                                         .raw     = 0x8385};
 
     ads1115_register_t config_reg_host;
+
     ads1115_decode_register(&config_reg_i2c, &config_reg_host);
 
     TEST_ASSERT_EQUAL_UINT8(ADS1115_REG_CONFIG, config_reg_host.address.P);
-    TEST_ASSERT_EQUAL_UINT8(ads1115_config_OS_DEFAULT, config_reg_host.config.OS);
-    TEST_ASSERT_EQUAL_UINT8(ads1115_config_MUX_DEFAULT, config_reg_host.config.MUX);
-    TEST_ASSERT_EQUAL_UINT8(ads1115_config_PGA_DEFAULT, config_reg_host.config.PGA);
-    TEST_ASSERT_EQUAL_UINT8(ads1115_config_MODE_DEFAULT, config_reg_host.config.MODE);
-    TEST_ASSERT_EQUAL_UINT8(ads1115_config_DR_DEFAULT, config_reg_host.config.DR);
-    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_MODE_DEFAULT, config_reg_host.config.COMP_MODE);
-    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_POL_DEFAULT, config_reg_host.config.COMP_POL);
-    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_LAT_DEFAULT, config_reg_host.config.COMP_LAT);
-    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_QUE_DEFAULT, config_reg_host.config.COMP_QUE);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_OS_DEFAULT,
+                            config_reg_host.config.OS);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_MUX_DEFAULT,
+                            config_reg_host.config.MUX);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_PGA_DEFAULT,
+                            config_reg_host.config.PGA);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_MODE_DEFAULT,
+                            config_reg_host.config.MODE);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_DR_DEFAULT,
+                            config_reg_host.config.DR);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_MODE_DEFAULT,
+                            config_reg_host.config.COMP_MODE);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_POL_DEFAULT,
+                            config_reg_host.config.COMP_POL);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_LAT_DEFAULT,
+                            config_reg_host.config.COMP_LAT);
+    TEST_ASSERT_EQUAL_UINT8(ads1115_config_COMP_QUE_DEFAULT,
+                            config_reg_host.config.COMP_QUE);
 }
