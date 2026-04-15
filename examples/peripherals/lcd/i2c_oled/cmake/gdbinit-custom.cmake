de31f60aa03e334b077cc4a01a7d59f0718989f4
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2026-04-14 21:37:34 -0700

    refactor(gdbinit): streamline GDB connect script generation process

--- examples/peripherals/lcd/i2c_oled/cmake/gdbinit-custom.cmake
+++ examples/peripherals/lcd/i2c_oled/cmake/gdbinit-custom.cmake
@@ -4,18 +4,14 @@ if(NOT DEFINED GDBINIT_CONNECT_PATH)
 endif()
 set(project_connect_path "${GDBINIT_CONNECT_PATH}")
 
-file(READ "${generated_connect_path}" generated_connect_content)
-string(REGEX REPLACE "(\r?\n)?continue\r?\n$" "" generated_connect_content
-    "${generated_connect_content}")
-string(REGEX REPLACE "(\r?\n)?target remote [^\r\n]+$" "" generated_connect_content
-    "${generated_connect_content}")
-
 if(EXISTS "${project_connect_path}")
+    message(STATUS "Using GDB connect script from ${project_connect_path}")
     file(READ "${project_connect_path}" project_connect_content)
     file(WRITE "${generated_connect_path}"
-        "${generated_connect_content}\n"
-        "${project_connect_content}\n"
-    )
+        "${project_connect_content}")
+    message(STATUS "Generated GDB connect script at ${generated_connect_path}")
 else()
-    file(WRITE "${generated_connect_path}" "${generated_connect_content}\n")
+    message(WARNING "GDB connect script not found at ${project_connect_path}; "
+        "skipping generation of GDB connect script")
 endif()
+
